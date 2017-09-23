a7b4d089d9f57a46341aad70590fbcf610f561f5
abylkasymovkuat <abylkasymov.kuat@gmail.com>
2017-09-24 01:25:15 +0600

    Second commit

--- SIS-Algo_and_DS/Training/BracketMatching/src/Main.java
+++ SIS-Algo_and_DS/Training/BracketMatching/src/Main.java
@@ -2,36 +2,79 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
+
+    class Node{
+        Node next;
+        char value;
+        public Node(char value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+
+    }
+    Node head;
+    int size= 0;
+    public Main(){
+        head = null;
+    }
+    public void insert(char value)
+    {
+        size++;
+        if ( head == null)
+        {
+            head = new Node(value);
+        }
+        Node oldHed= head;
+        head = new Node(value);
+        head.next = oldHed;
+
+    }
+    public char pop(){
+
+        size--;
+        char value = head.value;
+        head = head.next;
+        return value;
+    }
+    public char peek()
+    {
+        return head.value;
+    }
+    public boolean isEmpty()
+    {
+        return (size == 0);
+    }
     public static boolean CheckParentesis(String str)
     {
         if (str.isEmpty())
             return true;
 
-        Stack<Character> stack = new Stack<Character>();
+        Main main = new Main();
         for (int i = 0; i < str.length(); i++)
         {
             char current = str.charAt(i);
             if (current == '{' || current == '(' || current == '[')
             {
-                stack.push(current);
+                main.insert(current);
             }
 
 
             if (current == '}' || current == ')' || current == ']')
             {
-                if (stack.isEmpty())
+                if (main.isEmpty())
                     return false;
 
-                char last = stack.peek();
+                char last = main.peek();
                 if (current == '}' && last == '{' || current == ')' && last == '(' || current == ']' && last == '[')
-                    stack.pop();
+                    main.pop();
                 else
                     return false;
             }
 
         }
 
-        return stack.isEmpty();
+        return main.isEmpty();
     }
     public static void main(String args[])
     {
